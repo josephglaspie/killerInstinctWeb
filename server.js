724493b9e8929dba11a053063f69dc9fcad75812
Joseph Glaspie <josephglaspie@gmail.com>
2017-10-04 10:37:17 -0500

    port to PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 var logger = require('morgan');
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 
 app.use(logger("combined"));
